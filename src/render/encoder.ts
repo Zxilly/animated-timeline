0e569d9bf9f9e7daa064629bcb8c9e987b6f6444
Zxilly <zhouxinyu1001@gmail.com>
2024-05-08 06:07:54 +0800

    fix: support both correctly

--- src/render/encoder.ts
+++ src/render/encoder.ts
@@ -9,15 +9,20 @@ export type EncoderType = 'webp' | 'gif' | 'both'
 
 export function loadEncoder(type: EncoderType) {
   const encoders: Encoder[] = []
-  switch (type) {
-    case 'webp' || 'both':
-      encoders.push(new WebpEncoder())
-      break
-    case 'gif' || 'both':
-      encoders.push(new GifEncoder())
-      break
-    default:
-      throw new Error(`Unknown encoder type: ${type}`)
+  if (type === 'both') {
+    encoders.push(new WebpEncoder())
+    encoders.push(new GifEncoder())
+  } else {
+    switch (type) {
+      case 'webp':
+        encoders.push(new WebpEncoder())
+        break
+      case 'gif':
+        encoders.push(new GifEncoder())
+        break
+      default:
+        throw new Error(`Unknown encoder type: ${type}`)
+    }
   }
 
   if (debug) {
